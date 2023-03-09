e76d2dc2bc5d2c8e23ed0bd7a79e3b7e44a64cb6
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-03-09 13:58:16 +0300

    <feat: add swagger>

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmorateApplication {
-
+	//http://localhost:8080/swagger-ui/
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
